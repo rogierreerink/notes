e1f5007003411a39a074e68dd45e6bec740a6ceb
Rogier Reerink <me@rogier.io>
2025-07-06 16:22:38 +0200

    Initial API commit.

---
+++ notes-api/target/debug/deps/notes_api-a3310488ac7f11a8.d
@@ -0,0 +1,5 @@
+/Users/rogier/Documents/Code/Projects/notes/notes-api/target/debug/deps/libnotes_api-a3310488ac7f11a8.rmeta: src/main.rs
+
+/Users/rogier/Documents/Code/Projects/notes/notes-api/target/debug/deps/notes_api-a3310488ac7f11a8.d: src/main.rs
+
+src/main.rs:
