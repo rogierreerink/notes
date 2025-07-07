2ddf7fbb437c4583e0536a35859838263e705d74
Rogier Reerink <me@rogier.io>
2025-07-07 14:51:47 +0200

    Removed target dir.

--- notes-api/target/debug/deps/notes_api-04ad8b1b665a7d9d.d
+++
@@ -1,5 +0,0 @@
-/Users/rogier/Documents/Code/Projects/notes/notes-api/target/debug/deps/libnotes_api-04ad8b1b665a7d9d.rmeta: src/main.rs
-
-/Users/rogier/Documents/Code/Projects/notes/notes-api/target/debug/deps/notes_api-04ad8b1b665a7d9d.d: src/main.rs
-
-src/main.rs:
